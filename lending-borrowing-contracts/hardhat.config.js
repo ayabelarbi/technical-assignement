1fc15193fa98b1ae1524780c2a180c367c311f5f
aya <aya.belarbi16@gmail.com>
2024-07-29 00:51:20 +0300

    modif

---
+++ lending-borrowing-contracts/hardhat.config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
